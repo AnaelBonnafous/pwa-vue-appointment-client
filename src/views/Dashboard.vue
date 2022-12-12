ae97f4218c45d842e5b072aa0932f29f58170ef0
AnaelBonnafous <anael.bonnafous@gmail.com>
2022-12-12 16:33:34 +0100

    get appointments of current user

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { reactive } from "vue";
 
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, query, where } from "@firebase/firestore";
 import { firestore } from "@/plugins/firebase";
-import { getCurrentUser } from "vuefire";
+import { useCollection, useCurrentUser } from "vuefire";
 
 interface Appointment {
   date: Date | null;
@@ -19,28 +19,55 @@ const form = reactive<Appointment>({
   provider_uid: "",
 });
 
-const providers = [""];
+const user = useCurrentUser();
+
+const providerRef = collection(firestore, "provider");
+const providers = useCollection(providerRef);
+
+const appointmentRef = collection(firestore, "appointment");
+const appointments = useCollection(
+  query(appointmentRef, where("client_uid", "==", user.value?.uid || ""))
+);
 
 const createAppointment = async () => {
-  form.client_uid = (await getCurrentUser())?.uid;
-  await addDoc(collection(firestore, "appointment"), { ...form });
+  form.client_uid = user.value?.uid;
+  await addDoc(appointmentRef, { ...form });
 };
 </script>
 
 <template>
   <h1>Dashboard</h1>
-  <v-form @submit.prevent="createAppointment">
-    <v-text-field
-      v-model="form.date"
-      type="datetime-local"
-      label="Datetime"
-    ></v-text-field>
-    <v-select
-      v-model="form.provider_uid"
-      label="Provider"
-      :items="providers"
-    ></v-select>
-    <v-textarea v-model="form.description" label="Description"></v-textarea>
-    <v-btn type="submit" block>submit</v-btn>
-  </v-form>
+  <v-row>
+    <v-col>
+      <v-form @submit.prevent="createAppointment">
+        <v-text-field
+          v-model="form.date"
+          type="datetime-local"
+          label="Datetime"
+        ></v-text-field>
+        <v-select
+          v-model="form.provider_uid"
+          label="Provider"
+          :items="providers"
+          :item-value="(provider) => provider.uid || null"
+          :item-title="
+            (provider) => `${provider.firstname} ${provider.lastname}`
+          "
+        ></v-select>
+        <v-textarea v-model="form.description" label="Description"></v-textarea>
+        <v-btn type="submit" block>submit</v-btn>
+      </v-form>
+    </v-col>
+    <v-col>
+      <v-list>
+        <v-list-item
+          v-for="appointment in appointments"
+          :key="appointment.id"
+          :title="appointment.description"
+          subtitle="..."
+        >
+        </v-list-item>
+      </v-list>
+    </v-col>
+  </v-row>
 </template>
